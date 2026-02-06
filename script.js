615912b027133cb34ccc80864a5a75aa0284c6ce
GuilhermeCavalcante09 <guilherme.9035@colegioprogresso.g12.br>
2026-02-06 10:52:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const links = [
+    "https://exame.com/ciencia/robos-com-camuflagem-inspirada-em-polvos-podem-se-tornar-realidade/",
+    "https://exame.com/ciencia/as-inovacoes-que-marcaram-o-setor-espacial-em-2025/",
+    "https://exame.com/invest/mercados/certificou-pode-voar-quando-os-carros-voadores-entrarao-em-operacao/",
+    "https://exame.com/future-of-money/ia-seguranca-a-formula-inegociavel-para-o-setor-financeiro-2/"
+];
+
+const frame = document.getElementById("frame");
+const loading = document.getElementById("loading");
+
+function abrirLink(index) {
+    const url = links[index];
+
+    loading.style.display = "block";
+    loading.innerText = "Carregando conteúdo...";
+    frame.style.display = "none";
+
+    frame.src = url;
+
+    frame.onload = () => {
+        loading.style.display = "none";
+        frame.style.display = "block";
+    };
+}
