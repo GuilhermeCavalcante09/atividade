aafa4ab16bfeb5b4c92bea91c59f8c5941608d68
GuilhermeCavalcante09 <guilherme.9035@colegioprogresso.g12.br>
2026-02-08 20:10:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const links = [
     "https://exame.com/ciencia/as-inovacoes-que-marcaram-o-setor-espacial-em-2025/",
     "https://exame.com/invest/mercados/certificou-pode-voar-quando-os-carros-voadores-entrarao-em-operacao/",
     "https://exame.com/future-of-money/ia-seguranca-a-formula-inegociavel-para-o-setor-financeiro-2/"
+
+    "https://guilhermecavalcante09.github.io/qr-/"
 ];
 
 const frame = document.getElementById("frame");
@@ -22,3 +24,4 @@ function abrirLink(index) {
         frame.style.display = "block";
     };
 }
+
